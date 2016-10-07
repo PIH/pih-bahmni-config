efc517cee15b4bca184807f4fa6707676fa4d121
dmdesimone <Jan2014>
2016-10-07 14:51:48 -0400

    Civil Servant attributes

--- ces-config/openmrs/migrations/metadata/civil_status_coded_question.sql
+++ ces-config/openmrs/migrations/metadata/civil_status_coded_question.sql
@@ -1,79 +1,43 @@
--- This file is used to set up Occupation coded question concept
+-- This file is used to set up Civil Status coded question concept
 
 set @concept_id = 0;
 set @answer_concept_id = 0;
 set @concept_name_short_id = 0;
 set @concept_name_full_id = 0;
 
--- Occupation Coded Question
+-- Civil Status Coded Question
 call ensure_concept(@concept_id, '3cd6df26-26fe-102b-80cb-0017a47871b2', 'Civil Status','Civil Status', 'Coded', 'Question', false);
 set @parent_concept_id = @concept_id;
 
--- Occupation Person Attribute Type
-call ensure_person_attribute_type('civilStatus', 'Civil Status', 'org.openmrs.Concept', @parent_concept_id, 1, 1, 0, NULL, NULL, NULL, 5, 'c1f7d1f1-3f10-11e4-adec-0800271c1b75');
+-- Civil Status Person Attribute Type
+call ensure_person_attribute_type('civilStatus', 'Civil Status', 'org.openmrs.Concept', @parent_concept_id, 1, 1, 0, NULL, NULL, NULL, 5, 'a3c5921f-36c0-48d7-b96a-a88728e64208');
 
-
--- Commercial bike rider
-call ensure_concept(@concept_id, 'EAC97EE3-5059-4533-B544-6BC01218A6E9', 'Commercial bike rider','Commercial bike rider', 'N/A', 'Misc', false);
+-- Single
+call ensure_concept(@concept_id, 'df488243-d1d5-4b50-ae04-40b4ffdcf934', 'Single','Single', 'N/A', 'Misc', false);
 set @child_concept_id = @concept_id;
 call ensure_concept_answer (@parent_concept_id, @child_concept_id, 1);
 
--- Driver
-call ensure_concept(@concept_id, 'f15d07f0-be62-44d4-89ef-0e17e0aa480e', 'Driver','Driver', 'N/A', 'Misc', false);
+-- Married
+call ensure_concept(@concept_id, '3cee0aca-26fe-102b-80cb-0017a47871b2', 'Married','Married', 'N/A', 'Misc', false);
 set @child_concept_id = @concept_id;
 call ensure_concept_answer (@parent_concept_id, @child_concept_id, 2);
 
--- Farmer
-call ensure_concept(@concept_id, '3cd9757e-26fe-102b-80cb-0017a47871b2', 'Farmer','Farmer', 'N/A', 'Misc', false);
+-- Living with Partner
+call ensure_concept(@concept_id, '3cd6e96c-26fe-102b-80cb-0017a47871b2', 'Living with Partner','Living with Partner', 'N/A', 'Misc', false);
 set @child_concept_id = @concept_id;
 call ensure_concept_answer (@parent_concept_id, @child_concept_id, 3);
 
--- Health Worker
-call ensure_concept(@concept_id, '3cdbc91e-26fe-102b-80cb-0017a47871b2', 'Health care worker','Health care worker', 'N/A', 'Misc', false);
+-- Separated
+call ensure_concept(@concept_id, '3cd6e246-26fe-102b-80cb-0017a47871b2', 'Separated','Separated', 'N/A', 'Misc', false);
 set @child_concept_id = @concept_id;
 call ensure_concept_answer (@parent_concept_id, @child_concept_id, 4);
 
--- Housewife
-call ensure_concept(@concept_id, '3cda14e8-26fe-102b-80cb-0017a47871b2', 'Housewife','Housewife', 'N/A', 'Misc', false);
+-- Divorced
+call ensure_concept(@concept_id, '3cd6e55c-26fe-102b-80cb-0017a47871b2', 'Divorced','Divorced', 'N/A', 'Misc', false);
 set @child_concept_id = @concept_id;
 call ensure_concept_answer (@parent_concept_id, @child_concept_id, 5);
 
--- Military
-call ensure_concept(@concept_id, '034BCF1B-1D5A-4D80-87D3-35E440081A34', 'Military','Military', 'N/A', 'Misc', false);
+-- Widowed
+call ensure_concept(@concept_id, '3cd6e6f6-26fe-102b-80cb-0017a47871b2', 'Widowed','Widowed', 'N/A', 'Misc', false);
 set @child_concept_id = @concept_id;
 call ensure_concept_answer (@parent_concept_id, @child_concept_id, 6);
-
--- Miner
-call ensure_concept(@concept_id, '3cdcbd06-26fe-102b-80cb-0017a47871b2', 'Miner','Miner', 'N/A', 'Misc', false);
-set @child_concept_id = @concept_id;
-call ensure_concept_answer (@parent_concept_id, @child_concept_id, 7);
-
--- Pensioneer
-call ensure_concept(@concept_id, '3ce4113c-26fe-102b-80cb-0017a47871b2', 'Pensioneer','Pensioneer', 'N/A', 'Misc', false);
-set @child_concept_id = @concept_id;
-call ensure_concept_answer (@parent_concept_id, @child_concept_id, 8);
-
--- Police
-call ensure_concept(@concept_id, 'a1e44baf-c82f-4d4f-ac09-232da84c8a28', 'Police','Police', 'N/A', 'Misc', false);
-set @child_concept_id = @concept_id;
-call ensure_concept_answer (@parent_concept_id, @child_concept_id, 9);
-
--- Student
-call ensure_concept(@concept_id, '3cd976f0-26fe-102b-80cb-0017a47871b2', 'Student','Student', 'N/A', 'Misc', false);
-set @child_concept_id = @concept_id;
-call ensure_concept_answer (@parent_concept_id, @child_concept_id, 10);
-
--- Teacher
-call ensure_concept(@concept_id, 'd192f132-999a-4666-8725-028f9fcb8df4', 'Teacher','Teacher', 'N/A', 'Misc', false);
-set @child_concept_id = @concept_id;
-call ensure_concept_answer (@parent_concept_id, @child_concept_id, 11);
-
--- Trader
-call ensure_concept(@concept_id, '3cd9740c-26fe-102b-80cb-0017a47871b2', 'Trader','Trader', 'N/A', 'Misc', false);
-set @child_concept_id = @concept_id;
-call ensure_concept_answer (@parent_concept_id, @child_concept_id, 12);
-
--- Other
-call ensure_concept(@concept_id, '3cee7fb4-26fe-102b-80cb-0017a47871b2', 'Other','Other', 'N/A', 'Misc', false);
-set @child_concept_id = @concept_id;
-call ensure_concept_answer (@parent_concept_id, @child_concept_id, 13);
