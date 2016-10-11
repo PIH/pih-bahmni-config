aa77810b5c72025ae048b69109d454a90b3c6f38
dmdesimone <Jan2014>
2016-10-11 16:19:16 -0400

    test search

--- ces-config/openmrs/migrations/metadata/visits.sql
+++ ces-config/openmrs/migrations/metadata/visits.sql
@@ -2,6 +2,10 @@
 
 
 -- OPD Visit Type
-call ensure_visit_type('OPD', 'Visit for patients coming for OPD', '1C8B8D48-8001-4218-B96A-3C7EA38A63F7');
+call ensure_visit_type('General OPD', 'Visit for patients coming for OPD', '1C8B8D48-8001-4218-B96A-3C7EA38A63F7');
 
+call ensure_visit_type('Mental Health', 'Visit for patients coming for Mental Health service', '3ced9a68-26fe-102b-80cb-0017a47871b2');
 
+call ensure_visit_type('Maternal Health', 'Visit for patients coming for Maternal Health service', '159937AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
+
+call ensure_visit_type('Other', 'Visit for patients coming for Other service', '3cee7fb4-26fe-102b-80cb-0017a47871b2');
