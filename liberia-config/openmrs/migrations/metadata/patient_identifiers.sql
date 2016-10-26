df068f20947b1b95ec1fc59e044c4b4b70e22313
Cosmin Ioan <cioan@pih.org>
2016-10-26 11:17:45 -0400

    correct identifiers setup

--- liberia-config/openmrs/migrations/metadata/patient_identifiers.sql
+++ liberia-config/openmrs/migrations/metadata/patient_identifiers.sql
@@ -1,6 +1,6 @@
 -- This file is used to set up PATIENT_IDENTIFIER_TYPE in the system
 
 -- PATIENT_IDENTIFIER_TYPE
-call ensure_patient_identifier_type('Bahmni Id', 'Patient identifier type for use at Liberia Health Centers', 1, 'NOT_USED', 'FDC6DDD1-CE83-4814-9922-6E9AC0F1F443');
+call ensure_patient_identifier_type('Bahmni Id', 'Patient identifier type for use at Liberia Health Centers', 1, '7dfc1a64-e42f-11e5-8c3e-08002715d519');
 
-call ensure_idgen_identifier_source('FDC6DDD1-CE83-4814-9922-6E9AC0F1F443', 'Liberia', 'Local identifier generator for Liberia' ,'1234567890', '100001', 'Liberia', '', 9, 9, '586357AF-895A-4953-ACBD-8036214D46D6');
+call ensure_idgen_identifier_source('7dfc1a64-e42f-11e5-8c3e-08002715d519', 'LIB', 'Local identifier generator for Liberia' ,'1234567890', '100001', 'LIB', '', 9, 9, '586357AF-895A-4953-ACBD-8036214D46D6');
