ec548b5f9f3f8da813ad9c5c41f4161b794ec8b6
dmdesimone <Jan2014>
2016-10-26 15:04:50 -0400

    Add basic person attributes

--- liberia-config/openmrs/migrations/metadata/person_attribute_types.sql
+++ liberia-config/openmrs/migrations/metadata/person_attribute_types.sql
@@ -4,9 +4,9 @@
 call ensure_person_attribute_type('telephoneNumber', 'Telephone Number', 'java.lang.String', NULL, 1, 1,
     0, NULL, NULL, NULL, 1, 'f5eaf645-4c78-11e5-9192-080027b662ec');
 
--- Expediente SSA
-call ensure_person_attribute_type('Expediente_SSA', 'Expediente SSA', 'java.lang.String', NULL, 1, 1,
-    0, NULL, NULL, NULL, 2, 'ac6d9ae7-daf6-4a3c-848d-39d7f38656af');
+-- Mothers First Name
+call ensure_person_attribute_type('mothersFirstName', 'First Name of Mother', 'java.lang.String', NULL, 1, 1,
+    0, NULL, NULL, NULL, 2, '19379fd0-1d19-482f-b77e-2e84c4f8b960');
 
 -- Contact Name
 call ensure_person_attribute_type('contactName', 'Contact Name', 'java.lang.String', NULL, 1, 1,
@@ -16,11 +16,15 @@ call ensure_person_attribute_type('contactName', 'Contact Name', 'java.lang.Stri
 call ensure_person_attribute_type('contactRelationship', 'Contact Relationship', 'java.lang.String', NULL, 1, 1,
     0, NULL, NULL, NULL, 4, '3cd99a68-26fe-102b-80cb-0017a47871b2');
 
+-- Contact Address
+call ensure_person_attribute_type('contactAddress', 'Contact Address', 'java.lang.String', NULL, 1, 1,
+    0, NULL, NULL, NULL, 4, '5190CC3E-83F0-4410-8660-B109086D9A5E');
+
 -- Contact Phone Number
 call ensure_person_attribute_type('contactPhone', 'Contact Phone Number', 'java.lang.String', NULL, 1, 1,
     0, NULL, NULL, NULL, 5, '276f8057-55a4-4b1c-8915-69ad090fcffb');
 
--- retire givenNameLocal
+    -- retire givenNameLocal
 call ensure_person_attribute_type('givenNameLocal', 'First name in local language', 'java.lang.String', NULL, 0, 1,
     1, 1, now(), 'remove from registration screen', 5, '7e6db4ea-e42f-11e5-8c3e-08002715d519');
 
