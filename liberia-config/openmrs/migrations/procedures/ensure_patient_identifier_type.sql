0f2278c0208cbf9ea71267c4003727954d00003c
Cosmin Ioan <cioan@pih.org>
2016-10-26 11:35:15 -0400

    correct identifiers setup

--- liberia-config/openmrs/migrations/procedures/ensure_patient_identifier_type.sql
+++ liberia-config/openmrs/migrations/procedures/ensure_patient_identifier_type.sql
@@ -4,7 +4,6 @@ CREATE PROCEDURE ensure_patient_identifier_type (
   _name    VARCHAR(50),
   _description  TEXT,
   _required     TINYINT(1),
-  _locationbehavior varchar(50),
   _uuid    CHAR(38)
 
 )
@@ -15,9 +14,9 @@ BEGIN
   SELECT patient_identifier_type_id INTO _id FROM patient_identifier_type WHERE uuid = _uuid;
 
   IF (_id IS NULL) THEN
-    INSERT INTO patient_identifier_type(name, description, creator, date_created, required, location_behavior, uuid) VALUES (_name, _description, 1, now(), _required, _locationbehavior, _uuid);
+    INSERT INTO patient_identifier_type(name, description, creator, date_created, required, uuid) VALUES (_name, _description, 1, now(), _required, _uuid);
   ELSE
-    UPDATE patient_identifier_type SET name = _name, description = _description, required = _required, location_behavior = _locationbehavior WHERE uuid = _uuid;
+    UPDATE patient_identifier_type SET name = _name, description = _description, required = _required WHERE uuid = _uuid;
   END IF;
 
 END;
