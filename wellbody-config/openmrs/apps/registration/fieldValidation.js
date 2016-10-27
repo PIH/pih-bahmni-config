cebc276787a7b6141d311a6709727e87170ca6e5
dmdesimone <Jan2014>
2016-10-27 10:24:28 -0400

    Revert: make mother's name required

--- wellbody-config/openmrs/apps/registration/fieldValidation.js
+++ wellbody-config/openmrs/apps/registration/fieldValidation.js
@@ -3,12 +3,18 @@ customValidator = {
         method: function (name, value) {
             return value >= 0;
         },
-        errorMessage: "Age should be a pssitive value"
+        errorMessage: "Age should be a positive value"
     },
     "Telephone Number": {
         method: function (name, value, personAttributeDetails) {
             return value && value.length> 6;
         },
         errorMessage: "Telephone Number should have minimum 6 digits"
+    },
+    "caste": {
+        method: function (name, value, personAttributeDetails) {
+            return value.match(/^\w+$/);
+        },
+        errorMessage: "Caste should contain only alphanumeric characters"
     }
 };
