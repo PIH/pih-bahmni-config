97c1c7bbe63249ac231398a1bf046d5464774248
dmdesimone <Jan2014>
2016-10-27 10:18:31 -0400

    configure registration

--- wellbody-config/openmrs/apps/registration/fieldValidation.js
+++ wellbody-config/openmrs/apps/registration/fieldValidation.js
@@ -3,15 +3,12 @@ customValidator = {
         method: function (name, value) {
             return value >= 0;
         },
-        errorMessage: "Age should be a positive value"
+        errorMessage: "Age should be a pssitive value"
     },
     "Telephone Number": {
         method: function (name, value, personAttributeDetails) {
             return value && value.length> 6;
         },
         errorMessage: "Telephone Number should have minimum 6 digits"
-    },
-     "First Name of Mother": {
-        required: true
-        }
+    }
 };
