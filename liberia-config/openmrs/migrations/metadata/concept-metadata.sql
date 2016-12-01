4b219208e1ab3b872f782fca47e28c44b266ace9
mseaton <mseaton@pih.org>
2016-12-01 16:31:13 -0500

    Ensure we are setting up the "State" concept class before using it.

---
+++ liberia-config/openmrs/migrations/metadata/concept-metadata.sql
@@ -0,0 +1,7 @@
+-- This file should be used to set up all Concept Metadata in the system that is not present by default
+
+-- Concept classes
+
+call ensure_concept_class('State', 'Program states', '7ba99722-977c-4210-89ca-991ce3f3193c');
+
+
